b5d0fe49dab381d1b713eb0dd45772838e2efc89
Haborym666 <bastos.ricardo93@gmail.com>
2020-04-20 09:54:11 +0100

    Collision detection. Adding rows and column

--- js/main.js
+++ js/main.js
@@ -10,6 +10,21 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (var c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -32,10 +47,27 @@ function keyUpHandler(e) {
     }
 }
 
-function drawBall() {
+function collisionDetection() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                //console.log(b);
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
+function drawBall(color, bin) {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD"; //getRandomColor();
+    ctx.fillStyle = color;
+    if (bin == true)
+        ctx.fillStyle = color; 
     ctx.fill();
     ctx.closePath();
 }
@@ -48,10 +80,31 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for (var c = 0; c < brickColumnCount; c++) {
+        for (var r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
     /*Left and right limit*/
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -63,29 +116,39 @@ function draw() {
     else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
-            console.log("hit");
+            //dy = dy - 0.1;
+            //dx = dx - 0.1;
+            var bin = true;
+            console.log("hit", dx, dy);
         }
         else {
             alert("GAME OVER");
             document.location.reload();
             clearInterval(interval); // Needed for Chrome to end game
-            console.log(intreval);
         }
     }
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
-        console.log(paddleX)
     }
     else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
-        console.log(paddleX)
     }
 
-    x += dx;
-    y += dy;
+    x = x + dx;
+    y = y + dy;
 }
 
-var interval = setInterval(draw, 10);
+var interval = setInterval(draw, 20);
+
+function getRandomColor() {
+    var letters = "ABCDEF0123456789";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        console.log(color);
+    }
+    return color;
+}
 /*
 var lastCalledTime;
 var fps;
@@ -101,16 +164,7 @@ function requestAnimFrame() {
   lastCalledTime = Date.now();
   fps = 1/delta;
 }
-
-function getRandomColor() {
-    var letters = "ABCDEF0123456789";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-        console.log(color);
-    }
-    return color;
-}
-
-
 */
+
+
+
